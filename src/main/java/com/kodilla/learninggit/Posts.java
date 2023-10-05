fdb69afd7e4e64a5662ba98196105960c2118cd5
RafalNicki <Nicol79@op.pl>
2023-10-05 18:27:27 +0200

    first commit

---
+++ src/main/java/com/kodilla/learninggit/Posts.java
@@ -0,0 +1,13 @@
+package com.kodilla.learninggit;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Posts {
+    private String title;
+    private String content;
+}
---
+++ src/main/java/com/kodilla/learninggit/Users.java
@@ -0,0 +1,15 @@
+package com.kodilla.learninggit;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Users {
+    private String firstNane;
+    private String lastNane;
+    private int age;
+
+}
